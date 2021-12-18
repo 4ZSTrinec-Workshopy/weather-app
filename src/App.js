3f1d9186d26e9ba6a7bceb5359d85496909157eb
adam077x <adam.heczko@gmail.com>
2021-12-18 11:22:38 +0100

    Build Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: '',
+            image: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=New%20York&units=metric&APPID=822fc8446f5adc72ac8c766a871329a8`)
+        .then(res => res.json())
+        .then(result => {
+            if(result.cod !== "404") {
+                this.setState({ weather: result });
+                this.setState({ image: "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png" });
+            }
+        })
+    }
+
+    handleWeatherChange = (e) => {
+        if(e.key === "Enter") {
+            if(this.state.query === '') return;
+            
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.query}&units=metric&APPID=822fc8446f5adc72ac8c766a871329a8`)
+            .then(res => res.json())
+            .then(result => {
+                if(result.cod !== "404") {
+                    this.setState({ weather: result });
+                    this.setState({ image: "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png" });
+                    this.setState({query: ''});
+                }
+            })
+        }
+    }
+
+    render() {
+        if(!this.state.weather) return <p>"Loading..."</p>
+
+        return (
+            <div className="App">
+                <input className="locationInput" type="text" placeholder="Location..." onChange={e => this.setState({ query: e.target.value }) } value={this.state.query} onKeyPress={this.handleWeatherChange}/>
+                <div className="container2">
+                    <div className="location">{this.state.weather.name}, {this.state.weather.sys.country}</div>
+                    <div className="time"><img src={this.state.image}/>{this.state.weather.weather[0].main}</div>
+                </div>
+                <div className="container">
+                    <h1 className="degrees">{Math.round(this.state.weather.main.temp)}°C</h1>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
